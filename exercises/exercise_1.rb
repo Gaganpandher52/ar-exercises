495fec380834a3b835110e743da45fbea676fda5
Gaganpandher52 <gaganpandher52@gmail.com>
2019-06-12 01:33:50 +0000

    updated a store name by id

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,16 +4,18 @@ puts "Exercise 1"
 puts "----------"
 class Stores < ActiveRecord::Base
 
-    # allows .students method to be called on instance of cohort
-    has_many :stores # <= method call!
+    
   
   end
 # Your code goes below here ...
 # newstore = stores.new(name:'Burnaby', annual_revenue:  300000)
 
+
 Store.create(name: 'Burnaby', annual_revenue:  300000,mens_apparel:true,womens_apparel:true)
 Store.create(name: 'Richmond', annual_revenue:  1260000,mens_apparel:false,womens_apparel:true)
 Store.create(name: 'Gastown', annual_revenue:  190000,mens_apparel:true,womens_apparel:false)
+
+
 
 puts Store.count
 
